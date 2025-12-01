9959c578d9730c9b848824e8c2fef4603a6dafbc
Dugald Todd <dugaldtodd@gmail.com>
2025-12-02 00:12:23 +1000

    Remove ServiceArea and TrustBadges components from contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,11 @@
 import ContactHero from "@/components/ContactHero";
 import ContactForm from "@/components/ContactForm";
-import ServiceArea from "@/components/ServiceArea";
-import TrustBadges from "@/components/TrustBadges";
 
 export default function Contact() {
   return (
     <main className="min-h-screen">
       <ContactHero />
       <ContactForm />
-      <ServiceArea />
-      <TrustBadges />
     </main>
   );
 }
